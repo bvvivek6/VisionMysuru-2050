5f5a348726780c78e5b6e3fcfb09fc8dbe1fb7fb
bvvivek6 <vivekbv242813@gmail.com>
2026-01-05 21:19:04 +0530

    feat: add db connection

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const connectDB = require("./Utils/db");
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("common"));
 app.use(express.json());
+
+connectDB();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
