984a8a4aec97eadbf233684b1cd2a94b7a33916b
bvvivek6 <vivekbv242813@gmail.com>
2026-01-08 13:39:12 +0530

    refactor detailed view component

--- client/src/dashboard/DetailModal.jsx
+++ client/src/dashboard/DetailModal.jsx
@@ -28,39 +28,35 @@ const DetailModal = ({ submission, onClose, onUpdateStatus }) => {
           initial={{ opacity: 0, scale: 0.96 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.96 }}
-          className="relative max-h-[95vh] w-full max-w-4xl overflow-y-auto rounded-2xl bg-[var(--surface)] "
+          className="relative max-h-[90vh] w-full max-w-4xl overflow-y-auto rounded-xl bg-[var(--surface)] "
         >
           <div className="sticky top-0 z-10  bg-[var(--surface)] px-6 py-4 flex items-center justify-between">
             <div>
-              <h2 className="text-xl font-bold text-[var(--text)]">
+              <h2 className="text-xl  font-bold text-[var(--text)]">
                 {submission.solutionName}
               </h2>
-              <div className="mt-1 flex flex-wrap items-center gap-2 text-xs text-[var(--muted)]">
-                <span className="font-mono rounded bg-[var(--bg)] px-2 py-0.5">
+              <div className="mt-1 flex flex-wrap items-center gap-2  text-[var(--muted)]">
+                <span className="font-mono  text-white bg-black font-bold text-md  rounded bg-[var(--bg)] px-3 py-0.5">
                   {submission.teamId}
                 </span>
                 <span>•</span>
-                <span className="uppercase tracking-wide">
+                <span className="uppercase text-sm tracking-wide">
                   {submission.theme}
-                </span>
-                <span>•</span>
-                <span className="rounded bg-[var(--accent)]/10 px-2 py-0.5 text-[var(--accent)] font-semibold">
-                  {submission.status.replace("_", " ").toUpperCase()}
                 </span>
               </div>
             </div>
 
             <button
               onClick={onClose}
-              className="rounded-xl p-2 text-white bg-red-600 hover:bg-[var(--bg)] hover:text-red-500 transition"
+              className="rounded-2xl p-2 text-white bg-red-600 hover:bg-red-700  transition"
               aria-label="Close modal"
             >
               <FiX className="text-xl" />
             </button>
           </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 p-3">
-            <div className="lg:col-span-2 space-y-6">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 px-2 pb-6 pt-2">
+            <div className="lg:col-span-2 space-y-2">
               <Card title="Organization Details">
                 <InfoGrid
                   items={[
