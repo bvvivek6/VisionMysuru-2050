29cf8181a3901c2d077edc678ac6dff15405fbbc
bvvivek6 <vivekbv242813@gmail.com>
2026-01-09 09:09:50 +0530

    minor fix

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.use(helmet());
 app.use(cors());
-app.use(morgan("common"));
+app.use(morgan("dev"));
 app.use(express.json());
 const MONGO_URI = process.env.MONGO_URI;
 
